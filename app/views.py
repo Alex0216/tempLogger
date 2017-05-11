c7af2fe53ae4d93084041da8723e4caa40f84802
Alexandre St-Onge <stonge.alex@gmail.com>
2017-05-10 19:40:23 -0700

    added daterange picker

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from app import app
-from flask import render_template, g, request
+from flask import Flask, render_template, g, request, jsonify
 import sqlite3
 from dateutil.parser  import parse
 from datetime import datetime, timedelta
+import RPi.GPIO as GPIO
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -19,24 +20,39 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/index')
 def index(): 
-    fromDateString = request.args.get('from','')
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(18, GPIO.OUT)
+    GPIO.output(18, GPIO.HIGH)
     fromDate = datetime.now() - timedelta(days=1)
     toDate = datetime.now()
-    if fromDateString:
-        fromDate = parse(fromDateString)
-        toDateString = request.args.get('to', '')
-        if toDateString:
-            toDate = parse(toDateString)    
-            if toDate == fromDate:
-                toDate += timedelta(days=1)
-            print toDate
-            print fromDate
     
     cur = get_db().cursor()
-    cur.execute("SELECT * FROM temphum WHERE timestamp BETWEEN date(?) AND date(?) ORDER BY timestamp", (fromDate, toDate))
+    cur.execute("SELECT * FROM temphum WHERE timestamp BETWEEN ? AND ? ORDER BY timestamp", (fromDate.isoformat(), toDate.isoformat()))
     rows = cur.fetchall()
     rows.reverse()
     temperatures = [round(x[1],2) for x in rows]
     humidity = [round(x[2],2) for x in rows]
     timestamps = [x[0] for x in rows] #strftime("%Y-%b-%dT%H:%M")
+    GPIO.output(18, GPIO.LOW)
     return render_template('index.html',fromdate=fromDate, todate=toDate, labels=timestamps, temps=temperatures, hum=humidity)
+
+@app.route('/_get_data')
+def get_data():
+    if request.args.get('startDate') and request.args.get('endDate'):
+        startDate = request.args["startDate"]
+        endDate = request.args["endDate"]
+        print parse(startDate)
+        print parse(endDate)
+        fromDate = parse(startDate)
+        toDate = parse(endDate)
+        cur = get_db().cursor()
+        cur.execute("SELECT * FROM temphum WHERE timestamp BETWEEN ? AND ? ORDER BY timestamp", (fromDate.isoformat(), toDate.isoformat()))
+        rows = cur.fetchall()
+        rows.reverse()
+        temperatures = [round(x[1],2) for x in rows]
+        humidities = [round(x[2],2) for x in rows]
+        timestamps = [x[0] for x in rows] #strftime("%Y-%b-%dT%H:%M")
+        print timestamps
+        GPIO.output(18, GPIO.LOW)
+    return jsonify(timestamps=timestamps, temperatures=temperatures, humidities=humidities)
